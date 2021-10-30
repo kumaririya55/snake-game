2506a5dc452a209cfa4493691a737ee274132104
rocticstechnology <91675417+rocticstechnology@users.noreply.github.com>
2021-10-30 12:33:36 +0530

    Update snake game.py
    more optimized and fast

--- snake game.py
+++ snake game.py
@@ -2,6 +2,37 @@ import random
 import turtle
 import time
 
+# Creating a window screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("blue")
+
+# the width and height can be put as user's choice
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+# head of the snake
+head = turtle.Turtle()
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "Stop"
+
+
+# food in the game
+food = turtle.Turtle()
+colors = random.choice(['red', 'green', 'black'])
+shapes = random.choice(['square', 'triangle', 'circle'])
+food.speed(0)
+food.shape(shapes)
+food.color(colors)
+food.penup()
+food.goto(0, 100)
+
+
+pen = turtle.Turtle()
 
 class Square:
     def __init__(self, x, y):
@@ -43,6 +74,14 @@ class Food:
         # controls the blinking
         self.state = "OFF" if self.state == "ON" else "ON"
 
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 250)
+pen.write("Score : 0 High Score : 0", align="center",
+		font=("candara", 24, "bold"))
 
 class Snake:
     def __init__(self):
